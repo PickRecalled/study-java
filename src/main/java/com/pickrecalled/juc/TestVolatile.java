d31c764b0d7d12f0240ddbae2dab95d35656f4e4
pickrecalled <pickrecalled@163.com>
2018-10-23 09:06:17 +0800

    1501-JUC-volatile 关键字与内存可见性

---
+++ src/main/java/com/pickrecalled/juc/TestVolatile.java
@@ -0,0 +1,53 @@
+package com.pickrecalled.juc;
+
+public class TestVolatile {
+
+	public static void main(String[] args) {
+		// 启动线程修改值
+		ThreadDemo td = new ThreadDemo();
+		new Thread(td).start();
+
+		// 查看while的次数
+		long i = 1L;
+		// 判断标识是否被修改
+		while (true) {
+			// 使用了volatile关键字来替换synchronized（同步）锁
+			// synchronized (td) {
+			if (td.isFlag()) {
+				System.out.println("---------true--------");
+				break;
+				// }
+			}
+			i++;
+		}
+		System.out.println(i);
+	}
+
+}
+
+class ThreadDemo implements Runnable {
+
+	// 共享标识
+	private volatile boolean flag = false;
+
+	@Override
+	public void run() {
+		// 为了看到效果休眠200毫秒
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		flag = true;
+		System.out.println("flag=" + isFlag());
+	}
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+}
